16ca0b1c8da3f513b1060d851ddc68817e576e53
= <=>
2025-10-04 22:45:21 -0700

    create pages and routes for all pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
- import React from "react";
- const App = ()=>{
-  return(
-    <div className="bg-blue-600 text-gray-">
-      <h1 className="bg-blue-400 text-black ">Welocme to blog website</h1>
-    </div>
-  )
- }
- export default App
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/SignUp";
+import Projects from "./pages/Projects";
+
+import DashBoard from "./pages/DashBoard";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/"  element={<Home />} />
+        <Route path="/about"  element={<About />} />
+        <Route path="/signup"  element={<SignUp />} />
+        <Route path="/login"  element={<Login />} />
+        <Route path="/dashboard"  element={<DashBoard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+export default App;
