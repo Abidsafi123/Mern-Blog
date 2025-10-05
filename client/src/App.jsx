fdf7a239686adf86f1481b46c3db9f7b2e5e22cb
= <=>
2025-10-04 22:09:12 -0700

    create react js tailwind css and also create first template

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+ import React from "react";
+ const App = ()=>{
+  return(
+    <div className="bg-blue-600 text-gray-">
+      <h1 className="bg-blue-400 text-black ">Welocme to blog website</h1>
+    </div>
+  )
+ }
+ export default App
